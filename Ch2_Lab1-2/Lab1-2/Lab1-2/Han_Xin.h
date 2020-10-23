9abf1fbf49727186063eddbb2d0a857c1a8da4ec
nick123987456 <bbb14540@gmail.com>
2020-10-23 21:08:51 +0800

    Lab

---
+++ Ch2_Lab1-2/Lab1-2/Lab1-2/Han_Xin.h
@@ -0,0 +1,6 @@
+int Han_xin1();
+int Han_xin2();
+int Han_xin3();
+int Han_xin4();
+int Han_xin5();
+
---
+++ Ch2_Lab1-2/Lab1-2/Lab1-2/Main.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "Han_Xin.h"
+
+int main(void)
+{
+	Han_Xin1();
+	Han_Xin2();
+	Han_Xin3();
+	Han_Xin4();
+	Han_Xin5();
+
+	printf("\n");
+	system("pause");
+}
